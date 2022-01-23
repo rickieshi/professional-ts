8824fe1d53582bd37ea8591bda4b2818949fc3d4
Ricky Shi <shix@ae.com>
2022-01-23 18:13:38 -0500

    type guard/runtime checking

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,16 +1,34 @@
 import { apiCall } from '../utils/networking';
+import type { ITeam } from '../types';
 
-let cachedAllTeamsList: Promise<any[]>;
-export async function getAllTeams() {
+function isITeam (arg: any): arg is ITeam {
+  return (
+    typeof arg.iconUrl === 'string' &&
+    typeof arg.name === 'string' &&
+    typeof arg.id === 'string' &&
+    Array.isArray(arg.channels)
+  )
+}
+
+function assertIsTypedArray<T>(arg: any, check: (val: any) => val is T): asserts arg is T[] {
+  if (!Array.isArray(arg)) throw new Error(`Not an Error: ${JSON.stringify(arg)}`);
+  if(arg.some(item => !check(item))) throw new Error(`Violators found: ${JSON.stringify(arg)}`);
+}
+
+let cachedAllTeamsList: Promise<ITeam[]>;
+export async function getAllTeams(): Promise<ITeam[]> {
   if (typeof cachedAllTeamsList === 'undefined')
-    cachedAllTeamsList = apiCall('teams');
+    cachedAllTeamsList = apiCall('teams').then(raw => {
+      assertIsTypedArray(raw, isITeam);
+      return raw;
+    });
 
   return await cachedAllTeamsList;
 }
 
-const cachedTeamRecords: any = {};
+const cachedTeamRecords: Record<string, Promise<ITeam>> = {};
 
-export async function getTeamById(id: string) {
+export async function getTeamById(id: string): Promise<ITeam> {
   let cached = cachedTeamRecords[id];
   if (typeof cached === 'undefined')
     cached = cachedTeamRecords[id] = apiCall(`teams/${id}`);
